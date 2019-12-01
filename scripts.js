fd3f98ce8d9b2e2e70a6dbebf2c45b28d8571162
Ariel Huang <huangrr@umich.edu>
2019-12-01 01:01:01 -0500

    done css on index and press.html

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 function dropDown(){
 	document.querySelector("#drop-down").classList.toggle("show");
 }
+
+window.onscroll = function(){
+	var navi = document.querySelector(".navi");
+	var sticky = navi.offsetTop;
+	if (window.pageYOffset >= sticky) {
+    navi.classList.add("sticky");
+  } else {
+    navi.classList.remove("sticky");
+  }
+}
